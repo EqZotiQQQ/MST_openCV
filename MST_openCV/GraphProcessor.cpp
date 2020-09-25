e50523b5715cf97a73e12cc01256d09df8982b18
Michael Fedyakov <mf_52@mail.ru>
2020-09-26 00:57:02 +0300

    keep transfering from py; added node storage

--- MST_openCV/GraphProcessor.cpp
+++ MST_openCV/GraphProcessor.cpp
@@ -1,42 +1,69 @@
 #include "GraphProcessor.h"
 
 #include <iostream>
+#include <unordered_map>
+#include <functional>
+#include <memory>
+#include <utility>
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/core.hpp>
 
-GraphProcessor::GraphProcessor(const int weight, const int height, const std::string imageName) noexcept :
-    mImgRows(weight),
-    mImgCols(height),
-    mImgWindowName(imageName)
-    //mpImage(std::move(std::make_unique<cv::Mat>(mImgRows, mImgCols, CV_8UC3, cv::Scalar(0, 0, 0))))//idk why but it doesn't work
+GraphProcessor::GraphProcessor(const int weight, const int height, const std::string imageName) noexcept:
+        mImgRows(weight),
+        mImgCols(height),
+        mImgWindowName(imageName)
+        //mpImage(std::move(std::make_unique<cv::Mat>(mImgRows, mImgCols, CV_8UC3, cv::Scalar(0, 0, 0))))//idk why but it doesn't work
 {
-    mpImage = std::make_unique<cv::Mat>(mImgRows, mImgCols, CV_8UC3, cv::Scalar(0, 0, 0));
-    printf("Picture size: %d X %d\n", mpImage->rows, mpImage->cols);
+    mpImage =cv::Mat(mImgRows, mImgCols, CV_8UC3, cv::Scalar(0, 0, 0));
+    printf("Picture size: %d X %d\n", mpImage.rows, mpImage.cols);
 }
 
 GraphProcessor::~GraphProcessor() noexcept {
     std::cout << "destroyed" << std::endl;
 }
 
-void GraphProcessor::smMouseCallback(int event, int x, int y, int flags, void* param) noexcept {
-    printf("event! x: %d; y: %d\n", x, y);
+void GraphProcessor::smMouseCallback(int event, int x, int y, int flags, void* param) noexcept {    /*param - image*/
+    //printf("event! x: %d; y: %d\n", x, y);
+    auto graphProcesser = static_cast<GraphProcessor*>(param);
     if (event == cv::EVENT_LBUTTONDOWN) {
-    	
+        graphProcesser->process(x, y);
     }
+}
+
+
+//TODO: 1. calculate distances between nodes; 2. connect it.
+void GraphProcessor::process(const int x, const int y) noexcept {
+    tree.clear();
+    nodes.emplace_back(std::make_pair(x, y));
+    if(nodes.size() > 1) {
+        connectMST();
+    }
+    printNodes();
+}
+
+void GraphProcessor::connectMST() noexcept {
+    
 }
 
 int GraphProcessor::lunch() noexcept {
     auto lunch_status{ true };
     cv::namedWindow(mImgWindowName, cv::WINDOW_AUTOSIZE);
     while (lunch_status) {
-        cv::imshow(mImgWindowName, *mpImage);
-        cv::setMouseCallback(mImgWindowName, smMouseCallback, &*mpImage);
-        std::cout << "fuck" << std::endl;
+        cv::imshow(mImgWindowName, mpImage);
+        cv::setMouseCallback(mImgWindowName, smMouseCallback, this);    // 3rd param passes to callback
         auto c = cv::waitKey(0);
         if (c == 27) {
             lunch_status = false;
         }
     }
     return 0;
+}
+
+void GraphProcessor::printNodes() noexcept {
+    printf("count of nodes: %I64u\n", nodes.size());
+    for (const auto& i : nodes) {
+        printf("pair: %d %d\n", i.first, i.second);
+    }
 }
--- MST_openCV/GraphProcessor.h
+++ MST_openCV/GraphProcessor.h
@@ -3,19 +3,35 @@
 
 #include <opencv2/core/mat.hpp>
 
+#include <unordered_map>
 #include <functional>
 #include <memory>
+#include <iostream>
 
+#include <utility>
+
+class Coords {
+    int x;
+    int y;
+};
 
 class GraphProcessor {
 public:
+    using distance = double;
     GraphProcessor(const int weight = 320, const int height = 240, const std::string imageName = "image") noexcept;
     ~GraphProcessor() noexcept;
     static void smMouseCallback(int event, int x, int y, int flags, void* param) noexcept;
+    void connectMST() noexcept;
+    void process(const int x, const int y) noexcept;
+    void printNodes() noexcept;
     int lunch() noexcept;
 private:
-    std::unique_ptr<cv::Mat> mpImage;
+    cv::Mat mpImage;
     std::string mImgWindowName;
+    std::vector<std::pair<int,int>> nodes;
+    std::vector<std::pair<int,int>> tree;
+    std::vector<std::pair<int,int>> freeNodes;
+    //std::unordered_map<std::pair<int, int>, distance> mDistances;
     const int mImgRows;
     const int mImgCols;
 };
